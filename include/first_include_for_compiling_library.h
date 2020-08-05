481691983b776050db7e257de268ef39bee22a66
Davit Kalantaryan <davit.kalantar@desy.de>
2020-08-05 12:27:08 +0200

    added arm support

---
+++ include/first_include_for_compiling_library.h
@@ -0,0 +1,18 @@
+//
+// file:			first_include_for_compiling_library.h
+// created on:		2020 Aug 05
+// created by:		D. Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#ifndef FIRST_INCLUDE_FOR_COMPILING_LIBRARY_H
+#define FIRST_INCLUDE_FOR_COMPILING_LIBRARY_H
+
+
+//#ifdef _M_ARM
+//#include <arm_neon.h>
+//#elif defined(_M_ARM64)
+//#include <arm_neon.h>
+//#endif
+
+
+#endif // #ifndef FIRST_INCLUDE_FOR_COMPILING_LIBRARY_H
